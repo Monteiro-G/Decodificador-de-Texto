56a49689b7eb19e342348da8d806588514ab204c
Gilberto_Moura <gylberto120.gm@gmail.com>
2024-03-05 23:46:55 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+let resultado = '';
 function ocultarImagemTexto(){
   document.querySelector('.areaExibirResultado').style.backgroundImage ='none';
-
+  let ocultarMensag = document.querySelector('.textoInicial');
+  ocultarMensag.style.display = 'none';
 }
 
 function exibirImagemTexto(){
   const areaExibirResultado = document.querySelector('.areaExibirResultado').style.backgroundImage = 'url(/imgs/menu_1x.png)';
-
+  let mostrarMensag = document.querySelector('.textoInicial');
+  mostrarMensag.style.display = 'block';
 }
 
 function criptTexto() {
     let mensagemDigitada = document.querySelector('.mensagemRecebida').value;
+    limpar();
     let textoCriptografado = '';
 
     textoCriptografado = mensagemDigitada.replace(/e/g,'enter')
@@ -17,21 +21,23 @@ function criptTexto() {
                                          .replace(/a/g,'ai')
                                          .replace(/o/g,'ober')
                                          .replace(/u/g,'ufat');
-  return document.querySelector('.areaExibirResultado').innerText = textoCriptografado;
+  return resultado = document.querySelector('.areaExibirResultado').innerText = textoCriptografado;
 }
 criptTexto();
 
 function descriptTexto() {
     let mensagemCript = document.querySelector('.mensagemRecebida').value;
+    limpar();
     let textDescript = '';
     textDescript = mensagemCript.replace(/enter/g, 'e')
                                 .replace(/imes/g, 'i')
                                 .replace(/ai/g, 'a')
                                 .replace(/ober/g, 'o')
                                 .replace(/ufat/g, 'u');
-  return document.querySelector('.areaExibirResultado').innerText = textDescript;
+  return resultado = document.querySelector('.areaExibirResultado').innerText = textDescript;
+  
 }
-let resultado = descriptTexto();
+descriptTexto();
 function ctrlC() {
   let textoCopiado = document.querySelector('.areaExibirResultado').value;
     navigator.clipboard.writeText(textoCopiado).innerText;
@@ -40,4 +46,10 @@ function ctrlC() {
 function limpar() {
   let limpatexto = document.querySelector('.mensagemRecebida');
   limpatexto.value ='';
+}
+
+function ocultarResultado(){
+  resultado = document.querySelector('.areaExibirResultado');
+  resultado.innerText = '';
+  
 }
